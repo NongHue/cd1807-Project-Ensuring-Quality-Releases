ffc12e0a7ad0dc251d7869217a9a0a2efa1d16c3
nguyen khoa <nguyenkhoa@nguyens-MacBook-Air.local>
2023-01-01 11:35:36 -0800

    update webapp

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -6,16 +6,13 @@ resource "azurerm_service_plan" "test" {
   os_type             = "Windows"
 }
 
-resource "azurerm_linux_web_app" "test" {
+resource "azurerm_app_service" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  service_plan_id     = azurerm_service_plan.test.id
+  app_service_plan_id = azurerm_app_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
-  }
-  site_config {
-    always_on = false
   }
 }
